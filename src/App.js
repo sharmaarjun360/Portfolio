047965936393b7ab895c27f2dbffcd071d74cdd1
sharmaarjun360 <sharma.arjun360@gmail.com>
2021-03-07 16:56:22 -0800

    Project setup
    Deleted:
    public/favicon.ico
    public/logo192.png
    public/logo512.png
    Updated:
    package-lock.json
    package.json
    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-
+// /Users/arjunsharma/Documents/Projects/SorceTreeVault/Portfolio/node_modules/bootstrap/dist/css/bootstrap.min.css   
+import "bootstrap/dist/css/bootstrap.min.css"
 function App() {
   return (
    <h2> Portfolio project...</h2>
